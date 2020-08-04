dcbe2c70a1d1713d1a34c065326ef7dda40e40b4
Kostua <kostua.p@gmail.com>
2020-08-04 17:58:06 +0300

    New schema of load jobs

--- us2ua.groovy
+++ us2ua.groovy
@@ -1,36 +1,23 @@
 #!/usr/bin/env groovy
+ pipelineJob('JobsGenerator') {
 
-pipelineJob('example-pipeline') {
-  definition {
-    cps {
-      script('''
-        pipeline {
-          agent any
-            stages {
-              stage("Checkout") {
-		            steps {
-		                	git url: 'https://github.com/Kostua/us2ua-shipping-cost-calculator'
-		            }
-              stage ('Build') {
-                steps {
-                  echo 'Build phase'
-                }
+        def repo = 'https://github.com/Kostua/us2ua-shipping-cost-calculator.git'
+
+        description("Pipeline for $repo")
+
+        definition{
+          triggers {
+                  cron('H/5 * * * *')
               }
-              stage ('Unit tests') {
-                steps {
-                  echo 'Unit testing phase'
-                }
-              }
-              stage ('Deploy') {
-                steps {
-                  echo 'Deploy phase'
-                }
+          cpsScm {
+            scm {
+              git {
+                remote { url(repo) }
+                branches('master')
+                scriptPath('Jenkinsfile')
+                extensions { }
               }
             }
+          }
         }
-      '''.stripIndent())
-      sandbox()
-
-    }
-  }
-}
+      }
