cbc029e4fc3957d06d26bf4251b5fce6a9964303
Kostua <kostua.p@gmail.com>
2020-08-10 20:41:48 +0300

    New URL

--- us2ua.groovy
+++ us2ua.groovy
@@ -1,4 +1,4 @@
-pipelineJob('example-pipeline') {
+pipelineJob('us2ua-pipeline') {
   definition {
     cps {
       script('''
@@ -14,7 +14,7 @@ pipeline {
         stage('Build') {
             steps {
                 // Get some code from a GitHub repository
-                git 'https://github.com/jglick/simple-maven-project-with-tests.git'
+                git 'https://github.com/Kostua/us2ua-shipping-cost-calculator'
 
                 // Run Maven on a Unix agent.
                 sh "mvn -Dmaven.test.failure.ignore=true clean package"
