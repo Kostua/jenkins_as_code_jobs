e5c002a1053bdec65316e8da8cafb7e0356443f7
Kostua <kostua.p@gmail.com>
2020-08-31 10:22:01 +0300

    Little bit clearance

--- us2ua.groovy
+++ us2ua.groovy
@@ -18,12 +18,6 @@ pipelineJob('us2ua-pipeline') {
             steps {
                 // Get some code from a GitHub repository
                 git 'https://github.com/Kostua/us2ua-shipping-cost-calculator'
-
-                // Run Maven on a Unix agent.
-                sh "mvn -Dmaven.test.failure.ignore=true test"
-
-                // To run Maven on a Windows agent, use
-                // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
 
         }
