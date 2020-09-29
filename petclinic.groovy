3ab95fa6a94fcca0fd0df19f5facb5c6a4f0dab2
Kostua <kostua.p@gmail.com>
2020-09-29 23:33:58 +0300

    Petclinic job file initial commit

---
+++ petclinic.groovy
@@ -0,0 +1,43 @@
+pipelineJob('us2ua-pipeline') {
+  definition {
+    cps {
+      script('''
+pipeline {
+    agent any
+    options {
+        buildDiscarder(logRotator(numToKeepStr: '20'))
+        disableConcurrentBuilds()
+    }
+  
+    triggers {
+      pollSCM('H/5 * * * *') 
+    }
+    stages {
+        stage('Test') {
+            agent any
+            steps {
+                // Get some code from a GitHub repository
+                git 'https://github.com/spring-projects/spring-petclinic.git'
+
+                // Run Maven on a Unix agent.
+                sh "mvn -Dmaven.test.failure.ignore=true test"
+                // stash includes: '**/target/*.jar', name: 'app'
+
+            }
+        }
+
+    
+    post {
+        always {
+            echo 'One way or another, I have finished'
+            deleteDir() /* clean up our workspace */
+      }
+    }
+}
+
+'''.stripIndent())
+      sandbox()
+    }
+  }
+}
+queue('petclinic-pipeline')
