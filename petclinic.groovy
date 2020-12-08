ab6f363f81cbf15e87e11a7fc3b001c383b80bf7
Kostua <kostua.p@gmail.com>
2020-12-08 17:30:36 +0200

    Switched to git branchSorce

--- petclinic.groovy
+++ petclinic.groovy
@@ -1,9 +1,8 @@
 multibranchPipelineJob('peteclinic') {
     branchSources {
-        github {
+        git{
             id('123456789') // IMPORTANT: use a constant and unique identifier
-            repository('spring-petclinic')
-            repoOwner('Kostua')
+            remote('https://github.com/Kostua/spring-petclinic.git')
         }
     }
     orphanedItemStrategy {
